9bce8b4fa09e43c7203a83f35c30a830818b0245
dgknca <dgknca@gmail.com>
2020-10-18 20:38:43 +0300

    remove gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp')
 var plumber = require('gulp-plumber')
 var sass = require('gulp-sass')
-// var prefix = require("gulp-autoprefixer");
 var rename = require('gulp-rename')
 const cleanCSS = require('gulp-clean-css');
 
@@ -15,7 +14,6 @@ gulp.task('scss', function() {
   gulp
     .src(path.scss.source)
     .pipe(plumber())
-    // .pipe(prefix())
     .pipe(sass())
     .pipe(rename({ extname: '.css' }))
     .pipe(gulp.dest(path.scss.target))
